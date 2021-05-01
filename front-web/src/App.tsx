0f9d06b6bdf2809e0c6c9b5452fc0a9782b5b215
Michael Bryan <michaelbryan.eng@gmail.com>
2021-05-01 19:07:34 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
-
+import './core/assets/styles/custom.scss'
+import './app.scss';
+import Navbar from "./core/components/Navbar";
 const App = () => {
 
     return (
-         <h1>Hello</h1>    
+         <Navbar />
     );
 }
 
